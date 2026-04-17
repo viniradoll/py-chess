809ade979d74ce588c8c894430b6af6b2cd89db6
Vini <vinirnadolny@gmail.com>
2026-04-17 16:29:14 -0300

    pawn direction fix

--- src/chess/core/pieces/pawn.py
+++ src/chess/core/pieces/pawn.py
@@ -7,19 +7,20 @@ class Pawn(Piece):
 
     def getMoveList(self, board: board.BoardView, from_sq: datatypes.Square) -> list[datatypes.Move]:
         moveList: list[datatypes.Move] = []
-        newSquare = datatypes.Square(from_sq.row+1, from_sq.col)
+        direction = 1 if self.color == datatypes.Color.WHITE else -1
+        newSquare = datatypes.Square(from_sq.row+(1*direction), from_sq.col)
         if board.isAvailable(newSquare):
             moveList.append(datatypes.Move(from_sq,to_sq=newSquare))
         
-            newSquare = datatypes.Square(from_sq.row+2, from_sq.col)
+            newSquare = datatypes.Square(from_sq.row+(2*direction), from_sq.col)
             if not self.hasMoved and board.isAvailable(newSquare):
                 moveList.append(datatypes.Move(from_sq,to_sq=newSquare))
 
-        newSquare = datatypes.Square(from_sq.row+1, from_sq.col+1)
+        newSquare = datatypes.Square(from_sq.row+(1*direction), from_sq.col+1)
         if board.isCapturable(newSquare, self.color):
             moveList.append(datatypes.Move(from_sq,to_sq=newSquare))
 
-        newSquare = datatypes.Square(from_sq.row-1, from_sq.col+1)
+        newSquare = datatypes.Square(from_sq.row+(1*direction), from_sq.col-1)
         if board.isCapturable(newSquare, self.color):
             moveList.append(datatypes.Move(from_sq,to_sq=newSquare))
 
